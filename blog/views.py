212bff0adc5898db991af5783db5da957ad2d67c
Saroj Verma <sarojverma567@gmail.com>
2017-07-31 21:17:06 -0500

    Watson API added

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,45 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import Post
 from .forms import PostForm
+import json 
+from watson_developer_cloud import ToneAnalyzerV3 
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
 
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+    username='15de0e0e-00a5-429d-a446-f2a863cbf847',
+    password='ns8qSQMIlrpK',
+    version='2016-05-19')
+
+    language_translator = LanguageTranslator(
+    username='5a01c20d-fd8f-46ef-9af1-8d1a14c71ab8',
+    password='gNQ8Zq7Zj1sh',)
+    
+    #print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    for post in posts:
+        data = json.dumps(tone_analyzer.tone(text=post.text), indent=1)
+        j = json.loads(data);
+        post.info = j['document_tone']['tone_categories'][0]['tones']
+        #post.info = json.dumps(post.info);
+        post.angerScore = post.info[0]['score']
+        post.disgustScore = post.info[1]['score']
+        post.fearScore = post.info[2]['score']
+        post.joyScore = post.info[3]['score']
+        post.sadScore = post.info[4]['score']
+        #print(post.info[0]['tone_name'])
+        translation = language_translator.translate(
+        text=post.text,
+        source='en',
+        target='es')
+        post.translatedText = json.dumps(translation, indent=2, ensure_ascii=False)
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 # Create your views here.
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
